4017d6f90290375b0a970694358a536f6237ffb7
user <xydakov.andrey@gmail.com>
2024-07-21 14:21:41 +0300

    Настройка меню

--- alsa_settings_sound.py
+++ alsa_settings_sound.py
@@ -13,6 +13,9 @@ root.geometry("600x400")  # Устанавливаем размер окна
 menu_bar = tk.Menu(root)
 root.config(menu=menu_bar)
 
+#root.attributes("-alpha", 0.5)
+#root.attributes("-topmost", True)
+
 # Создаем выпадающее меню "File"
 file_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Меню", menu=file_menu)
@@ -23,8 +26,20 @@ file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit_app)
 
 # Создаем выпадающее меню "Help"
-help_menu = tk.Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Системная информация", menu=help_menu)
+#help_menu = tk.Menu(menu_bar, tearoff=0)
+help_menu = ttk.Notebook()
+help_menu.pack(expand=True, fill=BOTH)
+
+frame1 = ttk.Frame(help_menu)
+frame1.pack(fill=BOTH, expand=True)
+help_menu.add(frame1, text="Системная информация")
+label = ttk.Label(frame1, text=get_message_sys())
+label.pack()
+
+
+
+#menu_bar.add_cascade(label="Системная информация", menu=help_menu)
+#help_menu.add_radiobutton(label="hfnfnf", command=get_message_sys)
 #help_menu.add_command(label="", command=get_message_sys)
 
 
