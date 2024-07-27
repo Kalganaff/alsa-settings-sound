a84d4b9789cab763a717b70e5d325aeb0fa31156
user <xydakov.andrey@gmail.com>
2024-07-27 12:06:24 +0300

    Настройка меню

--- sysinfo.py
+++ sysinfo.py
@@ -11,6 +11,17 @@ def get_alsa_version():
     return result.stdout.decode('utf-8').strip()
 
 # Функция для получения названия звуковой карты
+def test_error_sound_card_name(command):
+    try:
+        output = subprocess.check_output(command, shell=True)
+        return output.decode('utf-8').strip()
+    except subprocess.CalledProcessError as e:
+        return f"Ошибка выполнения команды: {e}"
+
 def get_sound_card_name():
-    result = subprocess.run(['cat', '/proc/asound/cards'], stdout=subprocess.PIPE)
-    return result.stdout.decode('utf-8').strip()
+    command = "cat /proc/asound/card*/codec* | awk -F: '/Codec/{print $2}'"
+    output = test_error_sound_card_name(command)
+    return output
+
+print("Название звуковой карты:", get_sound_card_name())
+#Название звуковой карты: b'VIA VT1705'
