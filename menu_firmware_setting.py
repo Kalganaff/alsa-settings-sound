90a8f97b6f4e2d986a0708c9107c4394fd417193
user <xydakov.andrey@gmail.com>
2024-07-29 13:24:42 +0300

    обновление вывода информации с команд системы

--- menu_firmware_setting.py
+++ menu_firmware_setting.py
@@ -9,13 +9,32 @@ def firmware(frames):
     selected.set(optnum)
 
     frame = frames[Tabs.TAB2]
-    lbl = Label(frame, text="""Настройка конфигурации snd-intel-dsp 
-    Опция 1 позволяет использовать устаревший драйвер HDaudio. Это работает
-    для колонок и гарнитуры, но не позволяет DMIC( захват.""")
+    lbl = Label(frame,
+                text="""Настройка конфигурации snd-intel-dsp опция 1 позволяет использовать устаревший драйвер HDaudio. Это работает для колонок и гарнитуры, но не позволяет сделать DMIC(Digital Microphone) захват. В то же время опция 3 поддерживает DMIC, DSP(Digital Signal Processor) может обрабатывать звук более эффективно, SOF(Sound Open Firmware) драйверы обеспечивать лучшую совместимость с современными аудиоустройствами.""",
+                font=("Arial", 10),
+                background="white",
+                wraplength=590,
+                justify=LEFT)
 
-    rad1 = Radiobutton(frame, text='Опция - 1', value=1, variable=selected)
-    rad3 = Radiobutton(frame, text='Опция - 3', value=3, variable=selected)
-    lbl.grid(column=0, row=0)
-    rad1.grid(column=0, row=1)
-    rad3.grid(column=0, row=2)
+
+    rad1 = Radiobutton(frame,
+                       text="Опция - 1",
+                       value=1,
+                       variable=selected)
+
+    rad3 = Radiobutton(frame,
+                       text='Опция - 3',
+                       value=3,
+                       variable=selected)
+
+    lbl.grid(column=0,
+             row=0)
+
+    rad1.place(relx=0,
+               rely=0.4,
+               anchor="w")
+
+    rad3.place(relx=0,
+               rely=0.5,
+               anchor="w")
 
